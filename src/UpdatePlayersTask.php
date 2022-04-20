c6bc21a7860f57e87c17eca5297f534c102bcc43
YTBJero <mw.sevice@gmail.com>
2022-04-20 11:40:49 +0700

    Update UpdatePlayersTask.php

--- src/UpdatePlayersTask.php
+++ src/UpdatePlayersTask.php
@@ -52,7 +52,7 @@ class UpdatePlayersTask extends AsyncTask{
     }
 
     public function onCompletion() : void{
-	/** @var array<string, int> $res */
+	/** @var array|string|int $res */
         $res = $this->getResult();
 	$server = Server::getInstance();
         foreach(array($res['errors']) as $e){
