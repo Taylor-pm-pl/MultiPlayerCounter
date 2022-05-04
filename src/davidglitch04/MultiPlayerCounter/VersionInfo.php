4bef23e5279bc597f03216dec5e8fcd94379f6fa
YTBJero <nguyenhieucoder47@gmail.com>
2022-05-04 23:24:17 +0700

    VersionInfo

---
+++ src/davidglitch04/MultiPlayerCounter/VersionInfo.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace davidglitch04\MultiPlayerCounter;
+
+final class VersionInfo{
+
+	public const PLUGIN_VERSION = "0.0.5";
+	public const IS_DEVELOPMENT_BUILD = true;
+	public const BUILD_CHANNEL = "stable";
+}
