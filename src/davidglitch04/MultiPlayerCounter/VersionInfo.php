47738e4977adbc0152833178b70f223b4df437df
YTBJero <mw.sevice@gmail.com>
2022-05-04 23:32:37 +0700

    Release 0.0.5

--- src/davidglitch04/MultiPlayerCounter/VersionInfo.php
+++ src/davidglitch04/MultiPlayerCounter/VersionInfo.php
@@ -7,6 +7,6 @@ namespace davidglitch04\MultiPlayerCounter;
 final class VersionInfo{
 
 	public const PLUGIN_VERSION = "0.0.5";
-	public const IS_DEVELOPMENT_BUILD = true;
+	public const IS_DEVELOPMENT_BUILD = false;
 	public const BUILD_CHANNEL = "stable";
 }
